81903be5e360a8d9dcc428bb60d79b6e8f8134bf
ViniciusOliver <viniciuscesaroliveira1@gmail.com>
2023-09-03 17:07:03 -0300

    Create simple tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,8 +9,27 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function testStringInitWith(): void
     {
-        $this->assertTrue(true);
+        $this->assertStringStartsWith('fu', 'fulano');
+    }
+    public function testAdditionCommutativity(): void
+    {
+        $results1 = 10 + 20;
+        $results2 = 20 + 10;
+        $this->assertEquals($results1, $results2);
+    }
+    public function testPersonHasAttribute(): void
+    {
+        $person = [
+            'name' => 'Vinicius',
+            'city' => 'Guarapuava',
+        ];
+        $this->assertArrayHasKey('name', $person);
+    }
+    public function testStringContainsSymbol(): void
+    {
+        $string = "JS > PHP";
+        $this->assertStringContainsString('>', $string);
     }
 }
